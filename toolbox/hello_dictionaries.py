5652f7097a6e052d84668a2a598e111ed4f41d21
Mohammad Allahham <109595065+mohallahham@users.noreply.github.com>
2025-04-13 10:41:10 +0300

    questions for the quizz

---
+++ toolbox/hello_dictionaries.py
@@ -0,0 +1,67 @@
+questions = [
+    {
+        "question": "What is the tallest animal in the world?",
+        "options": ["A. Elephant", "B. Giraffe", "C. Kangaroo", "D. Ostrich"],
+        "answer": "B",
+        "type": "mcq",
+    },
+    {
+        "question": "What is the smallest planet in our solar system?",
+        "options": ["A. Mars", "B. Earth", "C. Mercury", "D. Pluto"],
+        "answer": "C",
+        "type": "mcq",
+    },
+    {
+        "question": "In which country would you find the city of Kyoto?",
+        "options": ["A. South Korea", "B. China", "C. Thailand", "D. Japan"],
+        "answer": "D",
+        "type": "mcq",
+    },
+    {
+        "question": "What color do you get when you mix red and white?",
+        "options": ["A. Pink", "B. Orange", "C. Purple", "D. Coral"],
+        "answer": "A",
+        "type": "mcq",
+    },
+    {
+        "question": "Who painted the Mona Lisa?",
+        "options": [
+            "A. Vincent Van Gogh",
+            "B. Pablo Picasso",
+            "C. Leonardo da Vinci",
+            "D. Michelangelo",
+        ],
+        "answer": "C",
+        "type": "mcq",
+    },
+    {
+        "question": "What is the capital city of Canada?",
+        "options": ["A. Toronto", "B. Montreal", "C. Vancouver", "D. Ottawa"],
+        "answer": "D",
+        "type": "mcq",
+    },
+    {
+        "question": "Which fruit has its seeds on the outside?",
+        "options": ["A. Strawberry", "B. Kiwi", "C. Blueberry", "D. Grapes"],
+        "answer": "A",
+        "type": "mcq",
+    },
+    {
+        "question": "Which ocean is the largest?",
+        "options": ["A. Atlantic", "B. Indian", "C. Arctic", "D. Pacific"],
+        "answer": "D",
+        "type": "mcq",
+    },
+    {
+        "question": "What is the name of the toy cowboy in Toy Story?",
+        "options": None,
+        "answer": "woody",
+        "type": "open",
+    },
+    {
+        "question": "Name one of the four houses at Hogwarts in the Harry Potter series.",
+        "options": None,
+        "answer": ["gryffindor", "hufflepuff", "ravenclaw", "slytherin"],
+        "type": "open",
+    },
+]
