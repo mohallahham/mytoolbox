f60813835fc49bf9b986a21e22e9d83a2b37f56c
Mohammad Allahham <109595065+mohallahham@users.noreply.github.com>
2025-07-06 14:17:19 +0300

    feat(pig_latin): add basic version

---
+++ exercises/beginner/05_pig_latin.py
@@ -0,0 +1,10 @@
+word = input("Enter a word to translate to pig latin ").lower()
+
+
+def pig_latin(word):
+    if word[0] in "aeiou":
+        return f"{word}way"
+    return f"{word[1:]}{word[0]}ay"
+
+
+print(pig_latin(word))
