2985ea5a675179528c640c4e87d406e763b5d9db
Mohammad Allahham <109595065+mohallahham@users.noreply.github.com>
2025-07-06 12:43:53 +0300

    refactor(hang_man): game logic into functions

--- exercises/intermediate/99_hang_man.py
+++ exercises/intermediate/99_hang_man.py
@@ -1,43 +1,59 @@
 import random
 
-with open("random_words.txt", "r") as file:
-    word_list = [line.strip() for line in file]
-word = random.choice(word_list)
-letters = []
-guessed_letters = []
-lives = len(word) + 1
 
-for _ in word:
-    letters.append("_")
-
-print("Can you guess the following word?")
+def load_words():
+    with open("random_words.txt", "r") as file:
+        return [line.strip() for line in file]
 
 
-while True:
-    if lives == 0:
-        print(f"sorry you have lost, and the word was {word}")
-        break
-    else:
-        print(" ".join(letters))
-        print(f"Guessed Letters : {' '.join(guessed_letters)}")
-        print(f"you have {lives} chances")
-        guess = input("Guess a letter ")
+def choose_word(word_list):
+    return random.choice(word_list)
 
-        if guess == "quit":
+
+def initialize_word(word):
+    return ["_" for _ in word]
+
+
+def display_word(word):
+    print("Can you guess the following word?")
+
+
+def play_game(word):
+    letters = initialize_word(word)
+    guessed_letters = []
+    lives = len(word) + 1
+    while True:
+        if lives == 0:
+            print(f"sorry you have lost, and the word was {word}")
             break
-
-        if guess in word:
-            for index, letter in enumerate(word):
-                if letter == guess:
-                    letters[index] = guess
-
-        elif guess in guessed_letters:
-            print(f"You have already guessed {guess}.")
-
         else:
-            lives -= 1
+            print(" ".join(letters))
+            print(f"Guessed Letters : {' '.join(guessed_letters)}")
+            print(f"you have {lives} chances")
+            guess = input("Guess a letter ")
+
+            if guess == "quit":
+                break
+
+            if guess in guessed_letters:
+                print(f"You have already guessed {guess}.")
+                continue
             guessed_letters.append(guess)
 
-        if "_" not in letters:
-            print("You have won")
-            break
+            if guess in word:
+                for index, letter in enumerate(word):
+                    if letter == guess:
+                        letters[index] = guess
+
+            else:
+                lives -= 1
+
+            if "_" not in letters:
+                print("You have won")
+                break
+
+
+word_list = load_words()
+word = choose_word(word_list)
+display_word(word)
+play_game(word)
